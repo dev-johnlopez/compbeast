187f4ca7a1052e4b2611b3d253945c0ae0546e99
John Lopez <johnlopez@Johns-MBP.attlocal.net>
2021-04-24 18:44:19 -0500

    updates

--- app/settings.py
+++ app/settings.py
@@ -11,35 +11,36 @@ from celery.schedules import crontab
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = 1
+DEBUG = os.getenv("DEBUG")
+FLASK_DEBUG = os.getenv("FLASK_DEBUG")
 uri = os.getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
+if uri is not None and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 SQLALCHEMY_DATABASE_URI = uri or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'asdfl;kh35128udafasdfasdf123'
-BCRYPT_LOG_ROUNDS = 13
+BCRYPT_LOG_ROUNDS = os.getenv("BCRYPT_LOG_ROUNDS")
 DEBUG_TB_ENABLED = DEBUG
-DEBUG_TB_INTERCEPT_REDIRECTS = False
-CACHE_TYPE = "simple"  # Can be "memcached", "redis", etc.
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-OAUTHLIB_INSECURE_TRANSPORT = 1
-REDIS_URL='redis://127.0.0.1:6379'
+DEBUG_TB_INTERCEPT_REDIRECTS = DEBUG
+CACHE_TYPE = os.getenv("CACHE_TYPE")
+SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
+OAUTHLIB_INSECURE_TRANSPORT = os.getenv("OAUTHLIB_INSECURE_TRANSPORT")
+REDIS_URL=os.getenv("REDIS_URL")
 CELERY_BROKER_URL = REDIS_URL
 CELERY_RESULT_BACKEND = REDIS_URL
-MAIL_SERVER = 'mail.privateemail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = 'admin@compbeast.gg'
-MAIL_PASSWORD = 'Saintviator1??'
-MAIL_DEFAULT_SENDER = 'admin@compbeast.gg'
-MAIL_DEBUG = os.environ.get('MAIL_DEBUG') or 1
-SERVER_NAME = 'compbeast.herokuapp.com'
+MAIL_SERVER = os.getenv("MAIL_SERVER")
+MAIL_PORT = os.getenv("MAIL_PORT")
+MAIL_USE_TLS = os.getenv("MAIL_USE_TLS")
+MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
+MAIL_DEBUG = os.environ.get('MAIL_DEBUG') or 0
+SERVER_NAME = os.getenv('SERVER_NAME')
 ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
 ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
 SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT')
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = os.getenv("SECURE_PROXY_SSL_HEADER")
+SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT")
 
 #CELERYBEAT_SCHEDULE = {
 #    'registration-reminder': {
