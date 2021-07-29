37fce9e23f128f1aa76e3e370145790f4d91896c
John Lopez <johnlopez@Johns-MBP.attlocal.net>
2021-07-29 17:23:00 -0500

    updates

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -242,6 +242,7 @@ def _update_cod_info_for_player(player):
         r = requests.get('https://frozen-island-36052.herokuapp.com/player_details?username={}'.format(player.username.replace("#", "%23")))
         data = json.loads(r.text)
         player.external_id = str(data['player'])
+        print("got external id - " + player.external_id)
         #print(str(data['profile']['lifetime']['mode']['br']['properties']['kdRatio']))
         # TODO - Test: player.kdr = int(data['kdr'])
         player.save()
