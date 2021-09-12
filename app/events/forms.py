85ade3b79d136eafd41f560ccb62678d1473721f
John Lopez <johnlopez@Johns-MBP.attlocal.net>
2021-09-12 09:28:33 -0500

    updates

--- app/events/forms.py
+++ app/events/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, FormField, BooleanField, \
                     TextAreaField, SelectField, FieldList, HiddenField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired, Optional, Email
+from wtforms.validators import DataRequired, Optional, Email, Length
 from app.events.models import Team, Player
 
 class ConfirmPlayerForm(FlaskForm):
@@ -14,9 +14,9 @@ class ConfirmPlayerForm(FlaskForm):
                                        **kwargs)
 
 class PlayerForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), length(max=80)])
-    email = EmailField('Email', validators=[DataRequired(), Email(), length(max=80)])
-    username = StringField('Activision Username', validators=[DataRequired(), length(max=80)])
+    name = StringField('Name', validators=[DataRequired(), Length(max=80)])
+    email = EmailField('Email', validators=[DataRequired(), Email(), Length(max=80)])
+    username = StringField('Activision Username', validators=[DataRequired(), Length(max=80)])
 
     def __init__(self, csrf_enabled=False, *args, **kwargs):
         super(PlayerForm, self).__init__(csrf_enabled=csrf_enabled,
@@ -24,7 +24,7 @@ class PlayerForm(FlaskForm):
                                        **kwargs)
 
 class TeamForm(FlaskForm):
-    name = StringField('Team Name', validators=[DataRequired(), length(max=30)])
+    name = StringField('Team Name', validators=[DataRequired(), Length(max=30)])
     players = FieldList(FormField(PlayerForm,
                                   default=lambda:
                                   Player()))
