cc861b7e79c11c0b8a103c6b9da69587ce75d8c2
John Lopez <johnlopez@Johns-MBP.attlocal.net>
2021-09-12 09:26:24 -0500

    updates

--- app/events/forms.py
+++ app/events/forms.py
@@ -14,9 +14,9 @@ class ConfirmPlayerForm(FlaskForm):
                                        **kwargs)
 
 class PlayerForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = EmailField('Email', validators=[DataRequired(), Email()])
-    username = StringField('Activision Username', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(), length(max=80)])
+    email = EmailField('Email', validators=[DataRequired(), Email(), length(max=80)])
+    username = StringField('Activision Username', validators=[DataRequired(), length(max=80)])
 
     def __init__(self, csrf_enabled=False, *args, **kwargs):
         super(PlayerForm, self).__init__(csrf_enabled=csrf_enabled,
@@ -24,7 +24,7 @@ class PlayerForm(FlaskForm):
                                        **kwargs)
 
 class TeamForm(FlaskForm):
-    name = StringField('Team Name', validators=[DataRequired()])
+    name = StringField('Team Name', validators=[DataRequired(), length(max=30)])
     players = FieldList(FormField(PlayerForm,
                                   default=lambda:
                                   Player()))
